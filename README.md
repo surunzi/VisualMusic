0a711f48f81603467b4bff2e816e2bf93cfb2632
surunzi <670785827@qq.com>
2014-05-15 02:27:21 +0800

    Add one more visual effect

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ HTML5 music visualization (Chrome only)
 Download the files directly or visit the page online [here](http://surunzi.github.io/VisualMusic/).
 
 ##How to use it?
-----------------
+
 - Drag the files into the browser or click the center to play the music.
 - Right click to open the song list panel.
 - Use arrow key **Left** and **Right** to switch visual effect.
---
+++ js/e7.js
@@ -0,0 +1,53 @@
+// 7号可视化效果
+define(['analyser', 'util'], function (analyser, util) {
+
+var canvas = util.getById('visual-canvas'),
+    ctx = canvas.getContext('2d'),
+    data, i, len, cx, cy, angle, beginAngle = 0, total,
+    len = analyser.getFftSize() / 16, r = 50,
+    twoPI = 2 * Math.PI,
+    angleGap = twoPI / 3,
+    color = 'rgba(186, 135, 72, 0.5)';
+    initOrNot = false;
+
+function draw() {
+    ctx.save();
+    data = analyser.getData();
+    angle = beginAngle;
+    cx = canvas.width / 2;
+    cy = canvas.height / 2;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.globalCompositeOperation = 'lighter';
+    ctx.strokeStyle = color;
+    ctx.lineWidth = 10;
+    total = 0;
+    for (i = 12; i < len; i += 2) {
+        angle += 0.2;
+        ctx.beginPath();
+        ctx.moveTo(cx + data[i] * Math.sin(angle), cy + data[i] * Math.cos(angle));
+        ctx.lineTo(cx + data[i] * Math.sin(angle + angleGap), cy + data[i] * Math.cos(angle + angleGap));
+        ctx.lineTo(cx + data[i] * Math.sin(angle + angleGap * 2), cy + data[i] * Math.cos(angle + angleGap * 2));
+        ctx.closePath();
+        ctx.stroke();
+        total += data[i];
+    }
+    beginAngle = (beginAngle + 0.00001 * total) % twoPI;
+    ctx.restore();
+}
+
+function init() {
+    util.setBg(7);
+    initOrNot = true;
+}
+
+function isInit() {
+    return initOrNot;
+}
+
+return {
+    draw: draw,
+    init: init,
+    isInit: isInit
+}
+
+});
